febe99bbd5a2353d39b4d4d7391db7c23667b285
PANKAJ SADHUKHAN <79303687+PANKAJ-SADHUKHAN@users.noreply.github.com>
2022-05-22 18:25:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/index.css" rel="stylesheet" >
+    <title>HealthWeb</title>
+</head>
+
+<header>
+    <div class="logo">
+        <img src="image/logo1.svg" alt="logoimage">
+    </div>
+        <div class="menu">
+            <ul>
+                <li><a href="#" id="m1">Home</a></li>
+                <li><a href="Prevention.php" id="m1">Prevention</a></li>
+                <li><a href="testing.php" id="m1">Testing</a></li>
+                <li><a href="aboutus.php" id="m1">About</a></li>
+
+            </ul>
+        </div>          
+        
+</header>
+<body>
+   
+    <div class="intro">
+            <label id="intro-1">
+                <h1>Welcome to HealthWeb</h1>
+                <p>Making Health
+                    Care Better Together
+                </p><br>
+               <p><a href="#" class="btn">Get Started</a></p>
+            <label>
+            
+    </div>
+    <div class="middle">
+        <div class="flow-img">
+            <div class="card">
+                <h3>Primary Care</h3>
+                <img src="image/h2.svg" class="card-img" alt="">
+                <p>Your home is the only palce from where you can
+                   start your primary care. To read more please click 
+                the link below.</p>
+                <a href="Readmore1.php" class="m5">Read More</a>
+            </div>
+        
+        
+            <div class="card">
+                <h3>Variants of Virus</h3>
+                <img src="image/undraw_medical_research_qg4d.svg" class="card-img"  alt="">
+                <p>Your home is the only palce from where you can
+                    start your primary care. To read more please click 
+                 the link below.</p>
+                <a href="Readmore2.php" class="m5">Read More</a>
+            </div>
+        
+            <div class="card">
+                <h3>Regular Fitness</h3>
+                <img src="image/undraw_healthy_lifestyle_re_ifwg.svg" class="card-img" alt="">
+                <p>Your home is the only palce from where you can
+                    start your primary care. To read more please click 
+                 the link below.</p>
+                <a href="Readmore3.php" class="m5">Read More</a>
+            </div>
+        </div>
+       
+    </div>
+    <footer>
+        <div class="contact">
+            <p>For more details follow us</p>
+        </div>
+    </footer>
+    <script type="text/script" src="/java script.js/home.js">
+        
+    </script>
+</body>
+</html>
